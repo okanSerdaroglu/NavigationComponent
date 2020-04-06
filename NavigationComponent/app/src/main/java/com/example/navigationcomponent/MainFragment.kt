e98071f91c3c2e4beb961aaefdb76df8cd4cb84b
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-04-06 22:24:14 +0300

    actions declared from main fragment

--- NavigationComponent/app/src/main/java/com/example/navigationcomponent/MainFragment.kt
+++ NavigationComponent/app/src/main/java/com/example/navigationcomponent/MainFragment.kt
@@ -5,55 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MainFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var buttonViewTransactions: Button
+    lateinit var buttonSendMoney: Button
+    lateinit var buttonViewBalance: Button
+    lateinit var navController: NavController // declare a NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_main, container, false)
+        buttonViewTransactions = view.findViewById(R.id.view_transactions_btn)
+        buttonSendMoney = view.findViewById(R.id.send_money_btn)
+        buttonViewBalance = view.findViewById(R.id.view_balance_btn)
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonViewTransactions.setOnClickListener(this)
+        buttonSendMoney.setOnClickListener(this)
+        buttonViewBalance.setOnClickListener(this)
+        navController =
+            Navigation.findNavController(view) // initialize NavController passing the view
+
+
     }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+            R.id.view_transactions_btn -> {
+                navController.navigate(R.id.action_mainFragment_to_viewTransactionsFragment)
+            }
+            R.id.send_money_btn -> {
+                navController.navigate(R.id.action_mainFragment_to_chooseRecipientFragment)
+            }
+            R.id.view_balance_btn -> {
+                navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+            }
+        }
+
+    }
+
 }
